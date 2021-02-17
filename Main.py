4017aad79b76abeb9dad951d37c16076a92da030
JoeOlafs <jaolafsson@hotmail.com>
2021-02-17 12:43:02 +0200

    Learning BeautifulSoup

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-"#BeautifulSoupExercise" 
+from bs4 import BeautifulSoup
+
+with open('index.html','r') as html_file:
+     content  = html_file.read()
+     
+     soup = BeautifulSoup(content, 'lxml')
+     navBar = soup.find_all('div', class_='Buttons')
+     for button in navBar:
+          button_name = button.a.text
+          print(button_name)
